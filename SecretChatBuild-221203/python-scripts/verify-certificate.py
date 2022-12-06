0ec1f1d796273015dd18b64dac545832fadc0b6c
이재용(jae yong, lee) <49151220+jae-yong-2@users.noreply.github.com>
2022-12-06 18:26:21 +0900

    Add files via upload

--- SecretChatBuild-221203/python-scripts/verify-certificate.py
+++ SecretChatBuild-221203/python-scripts/verify-certificate.py
@@ -2,6 +2,7 @@ from Crypto import Random
 from Crypto.Signature import pkcs1_15
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
+from Crypto.Util.Padding import unpad
 import base64, json
 
 def decode_base64(b64):
@@ -23,18 +24,19 @@ def read_as_json():
 # https://pycryptodome.readthedocs.io/en/latest/src/signature/pkcs1_v1_5.html
 def verify(hash, key, signature):
     # PKCS #1 v1.5 를 이용한 전자서명 검증, 성공시 True, 실패시 False 리턴
-    try:
-        pkcs1_15.new(key).verify(hash, signature)
-        return True
-        
-    except (ValueError, TypeError):
-        print ("The signature is not valid.")
-        return False
-
+	key = RSA.import_key(key)
+	try:
+		pkcs1_15.new(key).verify(hash, signature)
+		print ("The signature is valid.")
+		return True
+	except (ValueError, TypeError):
+		print ("The signature is not valid.")
+		return False
+	
 cert = read_as_json()
-print(cert["hash"])
-hash_compare = (cert["hash"])
-server_pubkey = cert["pubKey"]
+hash_compare = cert["hash"]
+server_pubkey = decode_base64(cert["pubKey"])
+print(server_pubkey)
 # bytes:서버 공개키 (HINT: JSON에는 BASE64 형태로 제공되어 있음)
 signature = cert["signature"]
 # bytes:서버 서명 (HINT: JSON에는 BASE64 형태로 제공되어 있음)
